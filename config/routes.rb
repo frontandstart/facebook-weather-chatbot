8eaf94d57da6a4a098ec2499007d9d3554d1e773
Roman Klevtsov <frontandstart@gmail.com>
2018-08-16 15:23:17 +0300

    add foutes fo fb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/facebook-messenger', to: 'users#facebook_messenger'
+  post '/facebook-messenger', to: 'users#facebook_messenger'
 end
