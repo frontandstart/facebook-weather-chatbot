4aec34fa25c066f5193542f79d8528066bc38bac
Roman Klevtsov <frontandstart@gmail.com>
2018-08-17 19:50:09 +0300

    set plain body to Httparty request

--- app/jobs/get_user_info_from_fb_job.rb
+++ app/jobs/get_user_info_from_fb_job.rb
@@ -3,7 +3,7 @@ class GetUserInfoFromFbJob < ApplicationJob
 
   def perform(facebook_id)
     user = User.find_by(facebook_id: facebook_id)
-    response = JSON.parse HTTParty.get( user.get_fb_info_path ), symbolize_names: true
+    response = JSON.parse HTTParty.get( user.get_fb_info_path, plain: true ), symbolize_names: true
     user.update(
       first_name: response[:first_name],
       last_name: response[:last_name],
