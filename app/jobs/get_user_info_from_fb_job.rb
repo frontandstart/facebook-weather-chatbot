76df08f7ca9a778f44fd8abd2b271b9ad89d7937
Roman Klevtsov <frontandstart@gmail.com>
2018-08-17 20:19:06 +0300

    set plain body to Httparty request

--- app/jobs/get_user_info_from_fb_job.rb
+++ app/jobs/get_user_info_from_fb_job.rb
@@ -3,7 +3,8 @@ class GetUserInfoFromFbJob < ApplicationJob
 
   def perform(facebook_id)
     user = User.find_by(facebook_id: facebook_id)
-    response = JSON.parse HTTParty.get( user.get_fb_info_path, body: {} ).body
+    response = JSON.parse HTTParty.get( user.get_fb_info_path, body: {}).body, symbolize_names: true
+    Rails.logger.debug "response #{response}"
     user.update(
       first_name: response[:first_name],
       last_name: response[:last_name],
