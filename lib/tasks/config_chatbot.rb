af20078dce484d789461c4813438026d2a915447
Roman Klevtsov <frontandstart@gmail.com>
2018-08-19 13:44:45 +0300

    add rake task to setup menu

---
+++ lib/tasks/config_chatbot.rb
@@ -0,0 +1,42 @@
+namespace :config do
+
+  desc 'Setup chatbot menu'
+
+  task chatbot: :environment do 
+    HTTParty.post( ENV['FB_API_PATH'] + '/me/messenger_profile',  
+      query: { access_token: ENV['FACEBOOK_MARKER_TESTIAMPOPUP_MESSENGER'] },
+      body: {
+        get_started: {
+          payload: 'get_started'
+        }   
+      }.to_json,
+      headers: { 'Content-Type'=>'application/json' }
+    )
+    
+    HTTParty.post( ENV['FB_API_PATH'] + 'me/messenger_profile',  
+      query: { access_token: ENV['FACEBOOK_MARKER_TESTIAMPOPUP_MESSENGER'] },
+      body: {
+        persistent_menu: [
+          {
+            locale: 'default',
+            composer_input_disabled: false,
+            call_to_actions: [
+              {
+                title: 'Weather Report',
+                type: 'postback',
+                payload: 'weather_report'
+              },
+              {
+                title: 'Edit Locationt',
+                type: 'postback',
+                payload: 'edit_location'
+              }
+            ]
+          }
+        ]
+      }.to_json,
+      headers: { 'Content-Type' => 'application/json' }
+    )
+  end
+
+end
