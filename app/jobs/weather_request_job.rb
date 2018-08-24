79852aa994a084019c55ef12ed7baaac7591d70b
Roman Klevtsov <frontandstart@gmail.com>
2018-08-24 19:40:09 +0300

    fix codestyle little-bit

--- app/jobs/weather_request_job.rb
+++ app/jobs/weather_request_job.rb
@@ -8,25 +8,24 @@ class WeatherRequestJob < ApplicationJob
       query: {
         lat: user.lat,
         lon: user.long,
-        'APPID': ENV['WEATHER_KEY'] 
+        'APPID': ENV['WEATHER_KEY']
       },
       headers: { 'Content-Type' => 'application/json' }
     ).body
+
     Rails.logger.debug "Get weather for User: #{user.id} response: #{response}"
     Rails.logger.debug "test str: #{response['cod']}"
-    if response['cod'].to_i == 200 
-      new_temperature = (response['main']['temp'].to_f - 273.15 ).round(2)
+
+    if response['cod'].to_i == 200
+      new_temperature = (response['main']['temp'].to_f - 273.15).round(2)
       user.update_temperature(new_temperature, response['name'])
       user.weather_message!(new_temperature, response['name']) if need_send
-    end
-    if response['cod'].to_i != 200
+    else
       SendFbMessageJob.perform_later(
         facebook_id,
-        {
-          text: I18n.t('bot.cant_get_weather_data')
-        }
+        { text: I18n.t('bot.cant_get_weather_data') }
       )
     end
-
   end
+
 end
