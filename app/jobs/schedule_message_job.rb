eaf06a8174e7a2b71a7599a1da1e88eaadb166d8
Roman Klevtsov <frontandstart@gmail.com>
2018-09-06 18:18:36 +0300

    up

--- app/jobs/schedule_message_job.rb
+++ app/jobs/schedule_message_job.rb
@@ -5,7 +5,7 @@ class ScheduleMessageJob < ApplicationJob
     # interval - in hours
     user = User.find_by(user_id: user_id)
     if user && user.daily_weather_report?
-      Message.weather_report_response(user)
+      Message.new.weather_report_response(user)
       user.scheduled_weather_report(interval)
     end
   end
